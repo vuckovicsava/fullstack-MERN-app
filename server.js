7f6efa36e08f7950ec95bb07f90ae1a11faa9432
Sava <sava.vuckovic@quantox.com>
2019-04-01 14:00:52 +0200

    connected to mongo db

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// database connection
+const db = require('./config/keys').mongoURI;
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log('database connection successful'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => res.send('djasflkasjf'));
 
 const port = process.env.PORT || 5555;
-
 app.listen(port, () => console.log(`Server running on port ${port}`));
