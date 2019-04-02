a81889cd204588c9df6a66f406413be0503c3e58
Sava <sava.vuckovic@quantox.com>
2019-04-02 11:20:39 +0200

    added profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,26 @@
 const router = require('express').Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
-router.get('/', (req, res) =>
-  res.json({
-    msg: 'Profile response'
-  })
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = 'There is no profile for this user';
+          return res.status(404).json(errors);
+        }
+
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
 );
 
 module.exports = router;
